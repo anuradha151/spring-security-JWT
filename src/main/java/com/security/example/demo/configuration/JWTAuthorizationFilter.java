6af0e462f59c261ddfa47a614e70c48604e32495
anuradha151 <anuranasinghe151@gmail.com>
2018-12-26 18:34:56 +0530

    spring security finalized

--- src/main/java/com/security/example/demo/configuration/JWTAuthorizationFilter.java
+++ src/main/java/com/security/example/demo/configuration/JWTAuthorizationFilter.java
@@ -1,12 +1,24 @@
 package com.security.example.demo.configuration;
 
+import com.security.example.demo.model.ApplicatioUser;
 import com.security.example.demo.service.UserService;
+import io.jsonwebtoken.Jwts;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static com.security.example.demo.configuration.SecurityConstant.*;
+
 public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
- private final UserService userService;
+    private final UserService userService;
 
     public JWTAuthorizationFilter(AuthenticationManager authenticationManager, UserService userService) {
         super(authenticationManager);
@@ -17,4 +29,30 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
         super(authenticationManager, authenticationEntryPoint);
         this.userService = userService;
     }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        String header = request.getHeader(HEADER_STRING);
+        if (header == null || !header.startsWith(TOKEN_PREFIX)) {
+            chain.doFilter(request, response);
+            return;
+        }
+        UsernamePasswordAuthenticationToken authenticationToken = getAuthenticationToken(request);
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        chain.doFilter(request, response);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthenticationToken(HttpServletRequest request) {
+        String token = request.getHeader(HEADER_STRING);
+        if (token == null) return null;
+        String userName = Jwts.parser()
+                .setSigningKey(SECRET)
+                .parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
+                .getBody()
+                .getSubject();
+        ApplicatioUser applicatioUser = userService.loadUserByUserName(userName);
+        return userName != null ? new UsernamePasswordAuthenticationToken(applicatioUser, null) : null;
+    }
+
 }
--- src/main/java/com/security/example/demo/configuration/SecurityConfig.java
+++ src/main/java/com/security/example/demo/configuration/SecurityConfig.java
@@ -1,18 +1,33 @@
 package com.security.example.demo.configuration;
 
+import com.security.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserService userService;
+
+    public SecurityConfig() {
+    }
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST,"/sign_up").permitAll()
+                .antMatchers(HttpMethod.POST, "/sign_up").permitAll()
                 .antMatchers("/*/floor1/**").hasRole("USER")
-                .antMatchers("/*/floor2/**").hasRole("ADMIN");
+                .antMatchers("/*/floor2/**").hasRole("ADMIN")
+                .and()
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager(), userService));
+
 
     }
 }
